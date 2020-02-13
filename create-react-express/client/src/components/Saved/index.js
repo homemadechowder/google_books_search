4ed5bf53b82f9c53dbab5bce213458899a946706
sreeveena <sreeveena21@gmail.com>
2020-02-12 21:35:55 -0800

    saved file

--- create-react-express/client/src/components/Saved/index.js
+++ create-react-express/client/src/components/Saved/index.js
@@ -6,15 +6,53 @@ import Card from "../Card";
 import CardList from "../CardList";
 
 
-function Saved() {
-  return (
-    <div>
-      <CardList>
-        <Card />
-      </CardList>
-    </div>
-  );
+class Saved extends Component {
+    state = {
+      books: [],
+      title: "",
+      author: "",
+      image: "",
+      description: ""
+    };
+    componentDidMount() {
+        this.loadBooks();
+      }
+      loadBooks = () => {
+        // API.getBooks()
+        //   .then(res =>
+        //     this.setState({ books: res.data, title: "", author: "", description: "" , image: ""})
+        //   )
+        //   .catch(err => console.log(err));
+      };
+    render(){
+        return (
+            <div>
+            
+            {this.state.books.length ? (
+              <CardList>
+                {this.state.books.map(book => (
+                  <Card key={book._id}>
+                    
+                    <h3>{book.title}</h3>
+                    <h5>Written by {book.author}</h5>  
+                      
+                    <img src=""/>
+                    <p>{book.description}</p>
+                    {/* <DeleteBtn onClick={() => this.deleteBook(book._id)} /> */}
+                  </Card>
+                ))}
+              </CardList>
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
+                
+           
+            </div>
+        );
+    }
+  
 }
+
 
 
 export default Saved;
