bc67ed1666255fdc2a775582a81dc369e28fa9f9
sreeveena <sreeveena21@gmail.com>
2020-02-16 18:59:00 -0800

    fixed the image link

--- create-react-express/client/src/components/Card/index.js
+++ create-react-express/client/src/components/Card/index.js
@@ -6,16 +6,16 @@ const cardStyle = {
 }
 
 function Card({
-   image,
-   title,
-   authors
-  }) {
+  image,
+  title,
+  authors
+}) {
   return <div className="card">
-            <img style = {cardStyle} class="card-img-top" src={image} alt="CardImage"></img>
-            <div className = "card-body">
-              <h5 class = "card-title">{title}</h5>  
-              <p class = "card-text">{authors}</p>
-            </div>
-          </div>
+    <img style={cardStyle} class="card-img-top" src={image} alt="CardImage"></img>
+    <div className="card-body">
+      <h5 class="card-title">{title}</h5>
+      <p class="card-text">{authors}</p>
+    </div>
+  </div>
 }
 export default Card;
--- create-react-express/client/src/components/Search/index.js
+++ create-react-express/client/src/components/Search/index.js
@@ -7,66 +7,75 @@ import Button from "../Button";
 
 
 class Search extends Component {
-  state = {
+
+  constructor(props) {
+    super(props);
+    this.state = {
       search: '',
       books: [],
       title: '',
-      authors: '', 
+      authors: '',
       synopsis: ''
-       
-    }
 
-
-  componentDidMount(){
-        this.searchBook("Harry potter");
-        console.log(this.state.books);
+    };
   }
 
-  searchBook(query){
-        API.getBook(query)
-           .then(res => this.setState({books: res.data.items}))
-           .catch(err => console.log(err))
+
+
+  componentDidMount() {
+    this.searchBook("Harry potter");
+    console.log(this.state.books);
   }
 
-  handleInputChange = event =>{
-        const {value, name} = event.target;
+  searchBook = (query) => {
+    API.getBook(query)
+      .then((res) => {
 
-        this.setState({
-            [name]:value
-        })
+        this.setState({ books: res.data.items });
+      })
+      .catch(err => console.log(err))
   }
 
-  onFormSubmit = event =>{
-        event.preventDefault();
-        this.searchBook('Harry potter');
-        this.printState(event);
+  handleInputChange = event => {
+    const { value, name } = event.target;
+
+    this.setState({
+      [name]: value
+    })
   }
 
-  printState(event){
-        event.preventDefault();
-        console.log(this.state);
-        console.log(this.state.books[0].volumeInfo.imageLinks.thumbnail);
+  onFormSubmit = event => {
+    event.preventDefault();
+    this.searchBook('Harry potter');
+    this.printState(event);
   }
-  
-  render(){
-    return( 
+
+  printState(event) {
+    event.preventDefault();
+    console.log(this.state);
+    console.log(this.state.books[0].volumeInfo.imageLinks.thumbnail);
+  }
+
+  render() {
+    return (
       <div>
-        <Button onClick = {this.onFormSubmit} />
-      <div>
+        <Button onClick={this.onFormSubmit} />
+        <div>
           {this.state.books.length ? (
-             <CardList>
+            <CardList>
               {this.state.books.map(book => (
-                  <div>
-                    <Card image = {'https://i.imgflip.com/prj6o.jpg'}title = {book.volumeInfo.title} authors = {book.volumeInfo.authors}/>
-                  </div>                  
+                <div>
+                  <Card image={book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1200px-No_image_available.svg.png"} title={book.volumeInfo.title} authors={book.volumeInfo.authors} />
+                </div>
               ))}
             </CardList>
           ) : (
-            <h3>No Results to Display</h3>
-          )}
+              <h3>No Results to Display</h3>
+            )}
+        </div>
       </div>
-      </div>
-      )}    
+    )
+  }
 }
 
 
