1fffb7c9eb59502d1256e4ecce6e5fc9b6f1a2da
sreeveena <sreeveena21@gmail.com>
2020-02-16 21:25:08 -0800

    saved page layout

---
+++ create-react-express/client/src/components/SaveCard/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const cardStyle = {
+  width: '100px',
+  height: '100px'
+}
+
+function Card({
+  image,
+  title,
+  authors,
+  description
+}) {
+  return <div className="card">
+    
+    <div className="card-body">
+      <h5 class="card-title">{title}</h5>
+      <button type="submit" className="search-button btn btn-primary ">Delete</button>
+      <button type="submit" className="search-button btn btn-primary mr-3">View</button>
+      <p class="card-text">{(authors) ? authors.map((author) =>(author+" ")) : ""}</p>
+    </div>
+    <div>
+    <img style={cardStyle} class="card-img-top" src={image} alt="CardImage"></img>
+    <p>{description}</p>
+    </div>
+  </div>
+}
+export default Card;
--- create-react-express/client/src/components/Saved/index.js
+++ create-react-express/client/src/components/Saved/index.js
@@ -8,11 +8,23 @@ import CardList from "../CardList";
 
 function Saved() {
   return (
-    <div>
-      <CardList>
-        <Card />
-      </CardList>
-    </div>
+      <div className="saved-results">
+        <h4 style={{paddingTop: 5, paddingLeft: 5}}>Saved Books</h4>
+        {/* <Button onClick={this.onFormSubmit} /> */}
+        <div className="card-dis">
+          {this.state.books.length ? (
+            <CardList>
+              {this.state.books.map(book => (
+                <div>
+                  <Card description={book.volumeInfo.description} image={book.volumeInfo.imageLinks ? book.volumeInfo.imageLinks.thumbnail : "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1200px-No_image_available.svg.png"} title={book.volumeInfo.title} authors={book.volumeInfo.authors} />
+                </div>
+              ))}
+            </CardList>
+          ) : (
+              <h3>No Saved books to Display</h3>
+            )}
+        </div>
+      </div>
   );
 }
 
